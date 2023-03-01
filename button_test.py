574b512c1b639a1f63d4e626b540372a3a70a227
Xf.W <xxw512@case.edu>
2023-02-28 21:51:50 -0500

    fix bugs

--- button_test.py
+++ button_test.py
@@ -6,34 +6,25 @@ from gpiozero import Button
 dht = adafruit_dht.DHT22(board.D14)
 button = Button(2)
 
-while True:
-    try:
-        temperature = dht.temperature
-        humidity = dht.humidity
-        # Print what we got to the REPL
-        print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
-    except RuntimeError as e:
-        # Reading doesn't always work! Just print error and we'll try again
-        print("Reading from DHT failure: ", e.args)
-
-def catch_temp()
-	try:
-		timestamp = time.asctime()
-		temperature = dht.temperature
-		humidity = dht.humidity
-		print("{}	Temperature:{}C	Humidity:{}%".format(timestamp, temperature, humidity))
-		print("-----------------------------------------------------------------------------")
-	except RuntimeError as error:
-		print("Failed to get temperature and humidity. Please try again...")
-		print(error)
-		print("=============================================================================")
-		return
-
 def main():
 	print("=============================================================================")
 	while True
 		button.when_pressed = catch_temp
 		time.sleep(1)	
+    def catch_temp()
+        try:
+            timestamp = time.asctime()
+            temperature = dht.temperature
+            humidity = dht.humidity
+            print("{}	Temperature:{}C	Humidity:{}%".format(timestamp, temperature, humidity))
+            print("-----------------------------------------------------------------------------")
+        except RuntimeError as error:
+            print("Failed to get temperature and humidity. Please try again...")
+            print(error)
+            print("=============================================================================")
+            return
+
+
 	
 	
 if __name__ == "__main__":
