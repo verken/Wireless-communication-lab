f7dc9b0a5765202aaf5b992ee7ffa9e0d7026279
Xf.W <xxw512@case.edu>
2023-03-01 02:58:53 -0500

    some new change

--- led2.py
+++ led2.py
@@ -3,18 +3,45 @@ import time
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT) # Red pin
-GPIO.setup(27, GPIO.OUT) # Green pin
-GPIO.setup(22, GPIO.OUT) # Blue pin
+GPIO.setup(11, GPIO.OUT) # Red pin
+GPIO.setup(10, GPIO.OUT) # Green pin
+GPIO.setup(9, GPIO.OUT) # Blue pin
 
 # Loop to cycle through colors
 while True:
-    GPIO.output(17, GPIO.HIGH) # Red
+    GPIO.output(11, GPIO.LOW) # 
+    GPIO.output(10, GPIO.HIGH) # 
+    GPIO.output(9, GPIO.HIGH) # 
     time.sleep(1)
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(27, GPIO.HIGH) # Green
+    GPIO.output(11, GPIO.HIGH) # 
+    GPIO.output(10, GPIO.LOW) # 
+    GPIO.output(9, GPIO.HIGH) # 
     time.sleep(1)
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(22, GPIO.HIGH) # Blue
+    GPIO.output(11, GPIO.HIGH) # 
+    GPIO.output(10, GPIO.HIGH) # 
+    GPIO.output(9, GPIO.LOW) # 
     time.sleep(1)
-    GPIO.output(22, GPIO.LOW)
+    GPIO.output(11, GPIO.HIGH) # 
+    GPIO.output(10, GPIO.HIGH) # 
+    GPIO.output(9, GPIO.HIGH) # 
+    time.sleep(1)
+    GPIO.output(11, GPIO.LOW) # 
+    GPIO.output(10, GPIO.LOW) # 
+    GPIO.output(9, GPIO.HIGH) # 
+    time.sleep(1)
+    GPIO.output(11, GPIO.HIGH) # 
+    GPIO.output(10, GPIO.LOW) # 
+    GPIO.output(9, GPIO.LOW) # 
+    time.sleep(1)
+    GPIO.output(11, GPIO.LOW) # 
+    GPIO.output(10, GPIO.HIGH) # 
+    GPIO.output(9, GPIO.LOW) # 
+    time.sleep(1)
+    GPIO.output(11, GPIO.HIGH) # 
+    GPIO.output(10, GPIO.HIGH) # 
+    GPIO.output(9, GPIO.HIGH) # 
+    time.sleep(1)
+    GPIO.output(11, GPIO.LOW) # 
+    GPIO.output(10, GPIO.LOW) # 
+    GPIO.output(9, GPIO.LOW) # 
+    time.sleep(1)
